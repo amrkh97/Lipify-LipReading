f7a8eb6aab6a4996081ecff88e11e141bfc59777
amrkh97 <amrkh97@gmail.com>
2020-02-12 11:50:24 +0200

    Modified ReadVid-test.py to generate the dataset
    - Added Function to segment a single video into its components
    - Added function to traverse all speakers and segment each of their videos.
    
    - Added function to generate the align file path

--- ReadVid-test.py
+++ ReadVid-test.py
@@ -24,10 +24,12 @@ adverbs = ['again', 'now', 'please', 'soon']
 alphabet = [chr(x) for x in range(ord('a'), ord('z') + 1)] 
 numbers = ['one','two','three','four','five','six','seven','eight','nine']
 ########################################################################################### 
-#WIP
+
 def getAlignFileName(Path):
     '''Helper function to get name of the align file for a certain video'''
-    return Path.split(".")[0] + ".align"    
+    tempPath = Path.split("/")
+    newPath = tempPath[0] + '/align/' + (tempPath[2].split('\\')[1]).split('.')[0] + '.align' 
+    return newPath  
     
     
 def extractWordTimingFromVideo(filename):
@@ -45,56 +47,64 @@ def cutVideo(videoPath,fileName,start_time,end_time):
                 new = video.subclip(start_time, end_time)
                 new.write_videofile(fileName)
 
-#WIP
-def segmentDataSet(Path, Number_Of_Files):
-    '''Function responsible for segmenting the whole dataset into separate word files '''
-    gen =  glob.iglob(Path+ "*.mpg")
-    for i in range(Number_Of_Files):
-        py = next(gen)
 
-#WIP
 def segmentSingleVideo(videoPath, alignFilePath):
-    
+    '''Function responsible for segmenting a single video into its underlying words in their prespective folders'''    
     wordTimings = extractWordTimingFromVideo(alignFilePath)
     for word in wordTimings:
-        print("Current Word is: {}".format(word.name))
+        #print("Current Word is: {}".format(word.name))
         start_time = round((float(word.start_time)/(FPS*1000)),3)
         end_time = round((float(word.end_time)/(FPS*1000)),3)
         
         if word.name in commands:
             new_index = len(os.listdir('Videos-After-Extraction/Commands/'))
-            fileName = 'Videos-After-Extraction/Commands/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Commands/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
         
         if word.name in prepositions:
             new_index = len(os.listdir('Videos-After-Extraction/Prepositions/'))
-            fileName = 'Videos-After-Extraction/Prepositions/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Prepositions/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
         
         if word.name in colors:
             new_index = len(os.listdir('Videos-After-Extraction/Colors/'))
-            fileName = 'Videos-After-Extraction/Colors/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Colors/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
         
         if word.name in numbers:
             new_index = len(os.listdir('Videos-After-Extraction/Numbers/'))
-            fileName = 'Videos-After-Extraction/Numbers/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Numbers/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
         
         if word.name in adverbs:
             new_index = len(os.listdir('Videos-After-Extraction/Adverb/'))
-            fileName = 'Videos-After-Extraction/Adverb/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Adverb/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
         
         if word.name in alphabet:
             new_index = len(os.listdir('Videos-After-Extraction/Alphabet/'))
-            fileName = 'Videos-After-Extraction/Alphabet/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Alphabet/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
         
         if word.name == 'sil':
             new_index = len(os.listdir('Videos-After-Extraction/Silence/'))
-            fileName = 'Videos-After-Extraction/Silence/{}.mp4'.format(new_index)
+            fileName = 'Videos-After-Extraction/Silence/{}_{}.mp4'.format(word.name, new_index)
             cutVideo(videoPath, fileName, start_time, end_time)
+    
+
+#WIP
+def segmentDataSet(Path, Number_Of_Speakers):
+    '''Function responsible for segmenting the whole dataset into separate word files '''
+    for i in range(Number_Of_Speakers):
+        videoPath = Path + "video/S{}/".format(i+1) + "*.mpg"
+        videosGen =  glob.iglob(videoPath)
+        try:
+            for j in range(len(os.listdir(videoPath.split('*')[0]))):
+                py = next(videosGen)
+                segmentSingleVideo(py,getAlignFileName(py))
+        except StopIteration:
+            print("Segmented the dataset.")
+    
     
 #Test function for videos to check that frames are correct    
 def getVideoFrames():
@@ -107,10 +117,9 @@ def getVideoFrames():
         count += 1
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    print(count)
-    pass    
+    print(count)    
 
 ####################################### MAIN CODE ############################################
-videoPath = 'DataSet-Trial/bbab8n.mpg'
-alignFilePath = 'DataSet-Trial/bbab8n.align'
-segmentSingleVideo(videoPath, alignFilePath)
+dataSetPath = 'GP DataSet/'
+numberSpeakers = 1
+segmentDataSet(dataSetPath,numberSpeakers)
