64a26556a72e8550740e1b36accb51623aa904ed
amrkh97 <amrkh97@gmail.com>
2020-02-23 17:40:20 +0200

    Added test code for detecting facial points

--- ReadVid-test.py
+++ ReadVid-test.py
@@ -1,15 +1,44 @@
 import cv2
 
-    
-#Test function for videos to check that frames are correct    
-def getVideoFrames():
-    vidcap = cv2.VideoCapture('Videos-After-Extraction/Adverb/0.mp4')
+
+def getVideoFrames(category, videoName):
+    vidcap = cv2.VideoCapture('Videos-After-Extraction/{}/{}.mp4'.format(category,videoName))
     success,image = vidcap.read()
-    count = 0
+    allFrames = []
+    allFrames.append(videoName.split('_')[0]) #Label
     while success:
-        cv2.imshow("frame%d.jpg" % count, image)     # save frame as JPEG file      
+        allFrames.append(image)
         success,image = vidcap.read()
-        count += 1
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    print(count)   
+    return allFrames   
+    
+#print(getVideoFrames('Alphabet','c_10596')[0])
+
+
+from imutils import face_utils
+import numpy as np
+import imutils
+import dlib
+import cv2
+
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("dlib-predictor.dat")
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread("test.jpg")
+
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# detect faces in the grayscale image
+rects = detector(image, 1)
+
+# loop over the face detections
+for (i, rect) in enumerate(rects):
+
+	shape = predictor(image, rect)
+	shape = face_utils.shape_to_np(shape)
+ 
+	for (x, y) in shape[49:]:
+		cv2.circle(image, (x, y), 2, (0, 0, 0))
+
+cv2.imshow("Output", image)
+cv2.waitKey(0)
