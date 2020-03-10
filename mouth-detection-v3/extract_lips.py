a7e7e4913a1de0d4e9c36cdee4f45dccfb7f87c7
amrkh97 <amrkh97@gmail.com>
2020-03-10 11:42:34 +0200

    Reformatted code and changed path to DLIB predictor

--- mouth-detection-v3/extract_lips.py
+++ mouth-detection-v3/extract_lips.py
@@ -1,34 +1,48 @@
-import cv2
-import numpy as np
 from FR import *
 from lip_detection import *
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 # function used to extract lips points
 # input: frame Name + extension
 # output: frame, mouth_roi points pair vector
-def extractLips(filename):
-    img = readFrame(filename)
-
-    detector,predictor = initlizeDlib()
+def extractLips(fileName):
+    img = readFrame(fileName)
+    detector, predictor = initializeDlib()
 
     resized = resizeImage(img)
 
-    frame, mouth_roi = lipDetection(resized, detector, predictor)
-    frame, mouth = mouthRegionExtraction(frame, mouth_roi)
-    return frame, mouth, mouth_roi
-#----------------------------------------------------------------------------
+    inputFrame, mouthROI = lipDetection(resized, detector, predictor)
+    inputFrame, mouthRegion = mouthRegionExtraction(inputFrame, mouthROI)
+    return inputFrame, mouthRegion, mouthROI
+
+
+def extractLipsFromFrames(inputFrame):
+    """Function to extract lips from a single frame"""
+    detector, predictor = initializeDlib()
+    resized = resizeImage(inputFrame)
+    inputFrame, mouthROI = lipDetection(resized, detector, predictor)
+    inputFrame, mouthRegion = mouthRegionExtraction(inputFrame, mouthROI)
+    return inputFrame, mouthRegion, mouthROI
+
+
+# ----------------------------------------------------------------------------
 # function used to extract lips region
 # input: frame, mouth_roi points pair vector
 # output: mouth region image
-def mouthRegionExtraction(frame, mouth_roi):
-    x0 = mouth_roi[0][0]
+def mouthRegionExtraction(inputFrame, mouthRoi):
+    x0 = mouthRoi[0][0]
     x0 = x0 - 10
-    y0 = mouth_roi[2][1]
+    y0 = mouthRoi[2][1]
     y0 = y0 - 10
-    x1 = mouth_roi[6][0]
+    x1 = mouthRoi[6][0]
     x1 = x1 + 10
-    y1 = mouth_roi[9][1]
+    y1 = mouthRoi[9][1]
     y1 = y1 + 10
-    mouth = frame[ y0 : y1 , x0 : x1 ]
-    return frame , mouth
+    mouthPart = inputFrame[y0: y1, x0: x1]
+    return inputFrame, mouthPart
+
+
+if "__main__" == __name__:
+    filename = "../test.jpg"
+    frame, mouth, mouth_roi = extractLips(filename)
--- mouth-detection-v3/lip_detection.py
+++ mouth-detection-v3/lip_detection.py
@@ -1,10 +1,8 @@
 import cv2
-import numpy as np
-import dlib
 
 
-def lipDetection(frame,detector,predictor):
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+def lipDetection(frame, detector, predictor):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
     if len(faces) == 0:
         return 0, 0
@@ -12,10 +10,9 @@ def lipDetection(frame,detector,predictor):
         face = faces[0]
         landmarks = predictor(gray, face)
         mouth_roi = []
-        for n in range(48,68):
+        for n in range(48, 68):
             x = landmarks.part(n).x
             y = landmarks.part(n).y
-            mouth_roi.append((x,y))
-            cv2.circle(frame,(x,y),3,(255,0,0),1)
+            mouth_roi.append((x, y))
+            # cv2.circle(frame, (x, y), 3, (255, 0, 0), 1)
         return frame, mouth_roi
-
