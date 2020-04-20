9302aa4617c6fe8799c6c373410ec4e4b7565b23
amrkh97 <amrkh97@gmail.com>
2020-04-20 21:45:03 +0200

    Modified Project's Prototype
    * The modification was to decrease runtime and make project ready for testing through connecting it with the folder where the server will send the files

--- project-prototype.py
+++ project-prototype.py
@@ -1,7 +1,8 @@
 """ This file contains a small demo for our project using
   only the CNN architectures. """
-import multiprocessing
-from itertools import product
+import glob
+# import os
+import time
 
 import cv2
 import numpy as np
@@ -14,11 +15,10 @@ import CommandModel
 import NumberModel
 import PrepositionModel
 from ConcatenateDataSet import getVideoFrames, extractLipsHaarCascade, stackFramesToImage
-from classLabels import getAllClassLabels
-import time
 
 
 def getTrainedModel(modelPath, modelCategory):
+    print("Fetching model weights...")
     modelDict = {'Adverb': AdverbModel.AdverbNet(),
                  'Alphabet': CharacterCNN.CharCNN(),
                  'Colors': ColorModel.ColorsNet(),
@@ -29,12 +29,14 @@ def getTrainedModel(modelPath, modelCategory):
     model = modelDict[modelCategory]
     model.Model.compile(optimizer="Adam", loss='categorical_crossentropy', metrics=['accuracy'])
     model.Model = tf.keras.models.load_model(modelPath + '/')
+    print("{} Model is loaded...".format(modelCategory))
     return model.Model
 
 
 def predictOneVideo(classDict, videoPath):
     # Model Loading:
     savedModelPath = 'C:/Users/amrkh/Desktop/SavedModels/'
+    videoPath = videoPath.replace("\\", "/")
     path = videoPath.split('/')
     categoryCNN = path[-1].split('_')[0]
     savedModelPath += categoryCNN
@@ -42,8 +44,9 @@ def predictOneVideo(classDict, videoPath):
     cnnModel = getTrainedModel(savedModelPath, categoryCNN)
 
     # Video Concatenation Operations:
+    print("Starting video preparation operations...")
     haarDetector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    videoFrames = getVideoFrames(videoPath)
+    videoFrames = getVideoFrames(videoPath)[:30]
     videoFrames = [extractLipsHaarCascade(haarDetector, x) for x in videoFrames]
     concatenatedImage = stackFramesToImage(videoFrames)
 
@@ -56,21 +59,50 @@ def predictOneVideo(classDict, videoPath):
     img = img / 255
 
     # Model Prediction:
+    print("Starting Model Prediction...")
     modelPrediction = cnnModel.predict_classes(img)
+    print(cnnModel.predict(img))
     dictForClass = list(dictForClass.items())
     prediction = [item for item in dictForClass if modelPrediction[0] in item][0][0]
 
     return prediction
 
 
+def createClassLabelsDict():
+    """Function to create the labels class dictionary
+     based on data generated from previous runs which will remain static"""
+    D = {'Prepositions': {'at': 0, 'by': 1, 'in': 2, 'with': 3},
+         'Numbers': {'eight': 0, 'five': 1, 'four': 2, 'nine': 3, 'one': 4,
+                     'seven': 5, 'six': 6, 'three': 7, 'two': 8},
+         'Commands': {'bin': 0, 'lay': 1, 'place': 2, 'set': 3},
+         'Colors': {'blue': 0, 'green': 1, 'red': 2, 'white': 3},
+         'Alphabet': {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7,
+                      'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14,
+                      'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21,
+                      'w': 22, 'x': 23, 'y': 24, 'z': 25},
+         'Adverb': {'again': 0, 'now': 1, 'please': 2, 'soon': 3}}
+
+    return D
+
+
 if __name__ == "__main__":
     start_time = time.time()
-    AllClassLabels = getAllClassLabels()
-    mylist = ['Colors_Blue.mp4', ]
-    # 'Colors_Blue.mp4']  # List of video paths to predict
+    # AllClassLabels = getAllClassLabels() # from classLabels import getAllClassLabels()
+    AllClassLabels = createClassLabelsDict()
+    receivedFilesFromServer = 'C:/Users/amrkh/Desktop/Projects/Lipify-server/uploads/*.mp4'
+    mylist = glob.glob(receivedFilesFromServer)
+    mylist.sort(key=lambda x: x.split('_')[-1])
+    result = []
 
-    with multiprocessing.Pool(processes=len(mylist)) as threadPool:
-        result = threadPool.starmap(predictOneVideo, product([AllClassLabels, ], mylist))
+    for video in mylist:
+        result.append(predictOneVideo(AllClassLabels, video))
 
+    result = " ".join(result)
     print(result)
+    predictionFilePath = 'C:/Users/amrkh/Desktop/Projects/Lipify-server/prediction.txt'
+
+    predictionFile = open(predictionFilePath, "w")  # write mode
+    predictionFile.write(result)
+    predictionFile.close()
+
     print("Run Time: {} Seconds".format(time.time() - start_time))
