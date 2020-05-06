5cfe3e4c364f5f2e454faa8acacc4ae32c43c225
Yozo <m.mufeed1996@gmail.com>
2020-05-06 14:12:20 +0200

    added model stats writing

--- mouth-detection-v4/main.py
+++ mouth-detection-v4/main.py
@@ -1,10 +1,13 @@
 import cv2
+import time
+import csv
 
 from FR import *
 from mouthDetection import *
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-videoPath = "../Prototype-Test-Videos/Colors_1.mp4"
+startTime = time.time()
+videoPath = "../Prototype-Test-Videos/Colors_7.mp4"
 frames = getVideoFrames(videoPath)
 detected = []
 for i, frame in enumerate(frames):
@@ -13,5 +16,12 @@ for i, frame in enumerate(frames):
     #img = resizeImage(img, dim=(150,100))
     detected.append(img)
     cv2.imshow(str(i), detected[-1])
+print("Run Time: {} Seconds".format(time.time() - startTime))
+with open('../Image-Processing-Test/ModelsTiming.csv', 'a', newline='') as f:
+    writer = csv.writer(f)
+    # writer.writerow(['Model', 'Video Name', 'Time Taken', 'Accuracy'])
+    vidName = videoPath.split('/')
+    writer.writerow(['HaarCascade', vidName[len(vidName)-1], time.time() - startTime, 90])
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
