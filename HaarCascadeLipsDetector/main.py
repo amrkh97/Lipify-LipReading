065cf5a4d4284f659e5abc5ea822a4d98c2f0ceb
Yozo <m.mufeed1996@gmail.com>
2020-05-10 03:47:21 +0200

    changed folder and file names + removed unneeded functions in harr cascade + fixed reading video for checking if not video is found

---
+++ HaarCascadeLipsDetector/main.py
@@ -0,0 +1,17 @@
+import cv2
+from FramesManipulations import *
+from HaarDetector import extractLipsHaarCascade
+
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+videoPath = "../Prototype-Test-Videos/Colors_2.mp4"
+frames, status = getVideoFrames(videoPath)
+if status == True:
+    detected = []
+    for i, frame in enumerate(frames):
+        img, status = extractLipsHaarCascade(faceCascade, frame)
+        detected.append(img)
+        cv2.imshow(str(i), detected[-1])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("error in getting video")
