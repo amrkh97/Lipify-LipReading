7555e8aa775e64fe5201a6ee1c16bef1eb2c62fc
Abdelgawad <elgawad.mostafa@gmail.com>
2020-07-26 14:04:16 +0200

    Feature Extraction using DCT and LDA added

---
+++ Feature Extraction/LDA/utils/data_operation.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+import sys
+
+def calculate_covariance_matrix(X, Y=None):
+    """ Calculate the covariance matrix for the dataset X """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
+
+    return np.array(covariance_matrix, dtype=float)
